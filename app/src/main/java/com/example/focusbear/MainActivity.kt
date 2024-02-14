de310e1978bbc017e58fec729edf80a3edfbff0a
a2ron-k <aaronkwah@gmail.com>
2024-02-14 11:03:44 +0100

    feat: add conditional logic to display text/image based on time

--- app/src/main/java/com/example/focusbear/MainActivity.kt
+++ app/src/main/java/com/example/focusbear/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.focusbear
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -9,7 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,16 +18,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
 import com.example.focusbear.ui.theme.FocusBearTheme
 import kotlinx.coroutines.delay
@@ -49,24 +49,31 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun Timer() {
     var time by remember {
-        mutableStateOf(0L)
+        mutableLongStateOf(0L)
     }
 
+    // Timer clock boolean status (running)
     var isStarted by remember {
         mutableStateOf(false)
     }
 
+    // Timer clock boolean status (stopped)
+    var isStopped by remember {
+        mutableStateOf(false)
+    }
+
+    var isReward by remember {
+        mutableStateOf(false)
+    }
+
     var startTime by remember {
-        mutableStateOf(0L)
+        mutableLongStateOf(0L)
     }
 
     val context = LocalContext.current
-
-    val keyboardController = LocalSoftwareKeyboardController.current
 
     Column(
         modifier = Modifier
@@ -75,8 +82,19 @@ fun Timer() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        // TODO - Update the time condition after development is done
+        if (isReward && time > 5000) {
+            // TODO - Replace the text field with an image
+            Text(text = "hi")
+        }
+
+        if (isReward && time < 5000) {
+            // TODO - Replace the text field with an image
+            Text(text = "ded")
+        }
+
         Text(
-            text = formatMilisec(timeInMilisec = time),
+            text = formatMS(timeInMS = time),
             style = MaterialTheme.typography.headlineMedium,
             modifier = Modifier.padding(10.dp)
         )
@@ -84,30 +102,58 @@ fun Timer() {
         Spacer(modifier = Modifier.padding(24.dp))
 
         Row {
-            Button(onClick = {
-                if (isStarted) {
+            if (isStarted && isStopped) {
+                // This part shows the reset
+                Button(onClick = {
+                    time = 0
                     isStarted = false
-                } else {
-                    startTime = System.currentTimeMillis() - time
-                    isStarted = true
-                    keyboardController?.hide()
+                    isStopped = false
+                    isReward = false
+                    startTime = 0 // Reset start time
+                }, modifier = Modifier.weight(1f)) {
+                    Text(text = "Reset")
                 }
-            }, modifier = Modifier.weight(1f)) {
+            } else if (isStarted && !isStopped) {
+                // This part shows the stop
+                Button(onClick = {
+                    isStopped = true // Only mark as stopped
+                    isReward = true
 
-                Text(text = if (isStarted) "Pause" else "Start", color = Color.White)
-
-            }
-
-            Spacer(modifier = Modifier.width(16.dp))
-
-            Button(onClick = { time = 0; isStarted = false }, modifier = Modifier.weight(1f)) {
-                Text(text = "Reset")
+                    // Check if elapsed time is greater than 15 minutes (900,000 milliseconds)
+                    // TODO - Update the time condition after development is done
+                    if (time > 5000) {
+                        // Display success message
+                        Toast.makeText(
+                            context,
+                            "Success! You focused for over 15 minutes.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        // Display fail message
+                        Toast.makeText(
+                            context,
+                            "Fail! You focused for less than 15 minutes.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }, modifier = Modifier.weight(1f)) {
+                    Text(text = "Stop")
+                }
+            } else {
+                // This part shows the start
+                Button(onClick = {
+                    isStarted = true
+                    isStopped = false
+                    startTime = System.currentTimeMillis() // Set start time to current time
+                }, modifier = Modifier.weight(1f)) {
+                    Text(text = "Start")
+                }
             }
         }
     }
 
-    LaunchedEffect(isStarted) {
-        while (isStarted) {
+    LaunchedEffect(isStarted && !isStopped) {
+        while (isStarted && !isStopped) {
             delay(1000)
             time = System.currentTimeMillis() - startTime
         }
@@ -116,20 +162,11 @@ fun Timer() {
 
 
 @Composable
-fun formatMilisec(timeInMilisec: Long): String {
-    val hours = TimeUnit.MILLISECONDS.toHours(timeInMilisec)
-    val mins = TimeUnit.MILLISECONDS.toMinutes(timeInMilisec) % 60
-    val secs = TimeUnit.MILLISECONDS.toSeconds(timeInMilisec) % 60
+fun formatMS(timeInMS: Long): String {
+    val hours = TimeUnit.MILLISECONDS.toHours(timeInMS)
+    val mins = TimeUnit.MILLISECONDS.toMinutes(timeInMS) % 60
+    val secs = TimeUnit.MILLISECONDS.toSeconds(timeInMS) % 60
 
     return String.format("%02d:%02d:%02d", hours, mins, secs)
 
-
 }
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    FocusBearTheme {
-//        Greeting("Android")
-//    }
-//}
